7878013e160ba46fbea2d58714bb549f4ec34f53
shreyasarunesh <shreyasarunesh161297@gmial.com>
2022-08-09 18:53:18 +0100

    Final index file

---
+++ Retriever/WikiIndexing/CreateIndex.py
@@ -0,0 +1,133 @@
+
+'''
+ *
+ * Name                   :   F21MP- Open Domain Question Answering (ODQA) Agent.
+ *
+ * Description            :  This class illustrates the process of creating an index.
+                            This includes initial and intermediate intermediate indexing
+
+                            This class uses SPIMI- Single-pass in-memory indexing algorithm
+ *
+ *
+ Indexing formate:      jonny-2314:t3b6i2r1;6432:t5c8b3i1;
+                        Ex.:- t3b6i2r1 represents token appears 3 times in title, 6 times in the body,
+                        2 times in infobox and 1time in reference.
+
+ *
+ * Author                 :   Shreyas Arunesh
+ *
+ *
+ * Reference              : https://github.com/neufang/wiki-Dump-Indexing/blob/master/buildInvertedIndexFromWikiDump.py
+'''
+
+
+import re
+from collections import defaultdict
+
+index_map = defaultdict(str)
+num_files = 0
+num_pages = 0
+id_title_map = {}
+
+
+class CreateIndex():
+
+    """
+       *
+       * Summary :    This class create the respective index field files of wikipedia dump.
+                       Ex - input text ----> index files
+       *
+       *
+       Args:            title, body, category, infobox, liks, reference.
+       *
+    """
+
+    def __init__(self, write_data):
+
+        self.write_data = write_data
+
+    '''
+     *
+     *  Summary : This creates the index wrt to the fields. 
+     *
+     *  Args    : Param -  title, body, category, infobox, link, reference
+     *
+     *  Returns : Creates the index.
+     *
+    '''
+
+    def index(self, title, body, category, infobox, link, reference):
+
+        global num_pages
+        global index_map
+        global id_title_map
+
+        words_set, title_dict, body_dict, category_dict, infobox_dict, link_dict, reference_dict = set(), defaultdict(
+            int), defaultdict(int), defaultdict(int), defaultdict(int), defaultdict(int), defaultdict(int)
+
+        words_set.update(title) # Index for title.
+        for word in title:
+            title_dict[word] += 1
+
+
+        words_set.update(body)  # Index for body.
+        for word in body:
+            body_dict[word] += 1
+
+
+        words_set.update(category)  # Index for category.
+        for word in category:
+            category_dict[word] += 1
+
+
+        words_set.update(infobox)   # Index for infobox.
+        for word in infobox:
+            infobox_dict[word] += 1
+
+
+        words_set.update(link)      # Index for wikipedia link.
+        for word in link:
+            link_dict[word] += 1
+
+
+        words_set.update(reference)     # Index for references.
+        for word in reference:
+            reference_dict[word] += 1
+
+        for word in words_set:
+            temp = re.sub(r'^((.)(?!\2\2\2))+$', r'\1', word)
+            is_rep = len(temp) == len(word)
+
+            if not is_rep:
+                posting = str(num_pages) + ':'
+
+                if title_dict[word]:
+                    posting += 't' + str(title_dict[word])
+
+                if body_dict[word]:
+                    posting += 'b' + str(body_dict[word])
+
+                if category_dict[word]:
+                    posting += 'c' + str(category_dict[word])
+
+                if infobox_dict[word]:
+                    posting += 'i' + str(infobox_dict[word])
+
+                if link_dict[word]:
+                    posting += 'l' + str(link_dict[word])
+
+                if reference_dict[word]:
+                    posting += 'r' + str(reference_dict[word])
+
+                posting += ';'
+
+                index_map[word] += posting
+
+        num_pages += 1
+
+        if not num_pages % 40000:
+            self.write_data.write_intermed_index()
+            self.write_data.write_id_title_map()
+
+            index_map = defaultdict(str)
+            id_title_map = {}
---
+++ Retriever/WikiIndexing/TextPreProcessor.py
@@ -0,0 +1,193 @@
+'''
+ *
+ * Name                   :   F21MP- Open Domain Question Answering (ODQA) Agent.
+ *
+ * Description            :  This class illustrates the text preprocessing while indexing Wikipedia dump
+ *
+ * Author                 :   Shreyas Arunesh
+ *
+ *
+ * Reference              : https://github.com/neufang/wiki-Dump-Indexing/blob/master/buildInvertedIndexFromWikiDump.py
+'''
+
+import re
+from collections import defaultdict
+
+index_map = defaultdict(str)
+num_files = 0
+num_pages = 0
+id_title_map = {}
+
+
+class TextPreProcessor:
+    """
+       *
+       * Summary :    This class takes the raw text as input and returns the preprocessed text the list of words.
+       *                Ex- raw text ----> list of words
+       Args: htms_tags
+            stem words
+            stop words
+       *
+    """
+
+    def __init__(self, html_tags, stem_words, stop_words):
+
+
+        self.html_tags = html_tags
+        self.stem_words = stem_words
+        self.stop_words = stop_words
+    '''
+     *
+     *  Summary : This function reomoves all the stopwords from the given text data by 
+                    iterating through the stopwords file
+     *
+     *  Args    : Param - text_data
+     *
+     *  Returns : returns cleaned data eliminating the  stop-words.
+     *
+    '''
+
+    def remove_stopwords(self, text_data):
+
+        cleaned_text = [word for word in text_data if word not in self.stop_words]
+
+        return cleaned_text
+    '''
+     *
+     *  Summary : This function iterates through the stem words file and returns the removes the sufix and prefix.
+     *
+     *  Args    : Param - single word 
+     *
+     *  Returns : returns cleaned word removing the stem words.
+     *
+    '''
+
+    def stem_word(self, word):
+
+        for wrd in self.stem_words:
+            if word.endswith(wrd):
+                word = word[:-len(wrd)]
+                return word
+
+        return word
+    '''
+     *
+     *  Summary : This function removes all the stopwords from the given text data by 
+                    iterating through the stem words in the given text. 
+     *
+     *  Args    : Param - text_data
+     *
+     *  Returns : returns cleaned word removing the stem words.
+     *
+    '''
+
+    def stem_text(self, text_data):
+
+        cleaned_text = [self.stem_word(word) for word in text_data]
+
+        return cleaned_text
+
+    '''
+       *
+       *  Summary : This function removes all the non ASCII characters using regular expression module
+       *
+       *  Args    : Param - text_data
+       *
+       *  Returns : returns cleaned data removing the ASCII characters. Ex:( " ",: ^ /) etc...
+       *
+      '''
+
+    def remove_non_ascii(self, text_data):
+
+        cleaned_text = ''.join([i if ord(i) < 128 else ' ' for i in text_data])
+
+        return cleaned_text
+
+    '''
+       *
+       *  Summary : This function removes all the HTML tags using regular expression module
+       *
+       *  Args    : Param - text_data
+       *
+       *  Returns : returns cleaned data removing the html tags. Ex:(<p>, <!--...-->, <hr>, <img>) etc ...
+       *
+      '''
+
+    def remove_html_tags(self, text_data):
+
+        cleaned_text = re.sub(self.html_tags, ' ', text_data)
+
+        return cleaned_text
+
+    '''
+       *
+       *  Summary : This function removes all the unnecessary empty space in the text data
+       *
+       *  Args    : Param - text_data
+       *
+       *  Returns : returns cleaned data removing the empty space. Ex:(' ')
+       *
+      '''
+
+    def remove_special_chars(self, text_data):
+
+        cleaned_text = ''.join(ch if ch.isalnum() else ' ' for ch in text_data)
+
+        return cleaned_text
+    '''
+       *
+       *  Summary : This function removes other special characters that links to external pages in the input text data. 
+       *
+       *  Args    : Param - text_data
+       *
+       *  Returns : returns cleaned data without links. Ex:(http, https)
+       *
+      '''
+    def remove_select_keywords(self, text_data):
+
+        text_data = text_data.replace('\n', ' ').replace('File:', ' ')
+        text_data = re.sub('(http://[^ ]+)', ' ', text_data)
+        text_data = re.sub('(https://[^ ]+)', ' ', text_data)
+
+        return text_data
+
+    '''
+       *
+       *  Summary : This function calls the above functions to remove non ascii character, 
+       *            remove html tags, remove special character.
+       *
+       *  Args    : Param - text_data
+       *
+       *  Returns : returns cleaned data.
+       *
+      '''
+
+    def tokenize_sentence(self, text_data, flag=False):
+
+        if flag:
+            text_data = self.remove_select_keywords(text_data)
+            text_data = re.sub('\{.*?\}|\[.*?\]|\=\=.*?\=\=', ' ', text_data)
+        cleaned_text = self.remove_non_ascii(text_data)
+        cleaned_text = self.remove_html_tags(cleaned_text)
+        cleaned_text = self.remove_special_chars(cleaned_text)
+
+        return cleaned_text.split()
+
+    '''
+       *
+       *  Summary : This is the main function to tokenize the input text data. 
+       *            This functions calls all the above functions 
+       *
+       *  Args    : Param - text_data
+       *
+       *  Returns : returns cleaned data. 
+       *
+      '''
+
+    def preprocess_text(self, text_data, flag=False):
+
+        cleaned_data = self.tokenize_sentence(text_data.lower(), flag)
+        cleaned_data = self.remove_stopwords(cleaned_data)
+        cleaned_data = self.stem_text(cleaned_data)
+
+        return cleaned_data
